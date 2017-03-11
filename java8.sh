80fbf626fe29dfd91f47dba77a16a6fa3d8761fd
Marius <mhognogi@users.noreply.github.com>
2017-03-11 17:32:37 +0200

    Create java8.sh

---
+++ java8.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# install java 8 on ubuntu
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+echo "Installed Java 8 success"
+java -version
