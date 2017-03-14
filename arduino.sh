402c1e366304a030c589005a6d54237e4ebbb6ba
Marius <mhognogi@users.noreply.github.com>
2017-03-14 20:38:21 +0200

    Update arduino.sh

--- arduino.sh
+++ arduino.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # First download Arduino zip in /home/yourUser/Downloads
 
+sudo apt-get update
 cd ~/Downloads    
 tar -xvf arduino-1.*.tar.xz    
 sudo mv arduino-1.*.1 /opt    
 cd /opt/arduino-1.*.1/    
 chmod +x install.sh    
 ./install.sh    
-
-exit
