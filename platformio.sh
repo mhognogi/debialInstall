55def45b8052ea06a5621d11986b87a610a57425
Marius <mhognogi@users.noreply.github.com>
2017-03-14 20:43:02 +0200

    Update platformio.sh

--- platformio.sh
+++ platformio.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+#prerequisite arduino full
 
-#first install atom ide
+#install atom ide
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get install atom
 
-#install platformio
-sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+#install manual package: platformio-ide (Setting/Packages/)
+#fix error
+#terminal: sudo chmod a+rw /dev/ttyUSB0
