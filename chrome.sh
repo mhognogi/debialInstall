607dd886390332f62592eb207bed7b96127c2c3b
Marius <mhognogi@users.noreply.github.com>
2017-03-16 18:08:33 +0200

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#install phpStorm from PhpStorm-2016.3.3.tar.gz
+#install google-chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
