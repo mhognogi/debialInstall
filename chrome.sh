13355430052a8220c87a77935ab46d8ae4dc1c6b
Marius <mhognogi@users.noreply.github.com>
2017-03-16 18:12:37 +0200

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -5,3 +5,6 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable
+
+#open google-chrome
+google-chrome
