777761d7636dba2b97686a933d0653ece522d0d0
Marius <mhognogi@users.noreply.github.com>
2017-03-16 09:33:06 +0200

    Update chromium.sh

--- chromium.sh
+++ chromium.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# install chromium browser on ubuntu
 
+# install chromium browser on ubuntu
 sudo apt-get update
 sudo apt-get install chromium-browser
+
+#open chromium-browser
+chromium-browser
