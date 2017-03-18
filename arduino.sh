abbc2e01314f233fe86a96c7cbf2ba87758c98fc
Marius <mhognogi@users.noreply.github.com>
2017-03-18 14:56:05 +0200

    Update arduino.sh

--- arduino.sh
+++ arduino.sh
@@ -9,8 +9,23 @@ cd /opt/arduino-1.*.1/
 chmod +x install.sh    
 ./install.sh  
 
+read pa
+echo 
+echo "- For continu install"
+echo "need make some setting in Arduino IDE, after which we will proceed with the installation dependencies on github"
+echo "for contunuu open Arduino IDE and put in Menu File/Prefernces/Additional Board Manager: "
+echo "http://arduino.esp8266.com/stable/package_esp8266com_index.json"
+echo "and in Menu Tools/Board/Board Manager click and install package (in my case esp8266 by ESP8266 Community)"
+echo "after close Arduino IDE and press y in terminal"
+echo 
+echo "- For exit to install press n"
 #manual put in: File/Prefernces/Additional Board Manager URLs: http://arduino.esp8266.com/stable/package_esp8266com_index.json
 #Arduino IDE Tools/Board/Board Manager and install package (in my case esp8266 by ESP8266 Community)
+if [ "$pa" == "y" ]; then
+echo "Continuu with git dependece";
+else
+exit;
+fi
 
 cd ~/Arduino
 mkdir hardware
