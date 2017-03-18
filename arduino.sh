478bb1b7619a1894b44ac5bde241756eb9505aee
Marius <mhognogi@users.noreply.github.com>
2017-03-18 15:01:04 +0200

    Update arduino.sh

--- arduino.sh
+++ arduino.sh
@@ -12,14 +12,14 @@ chmod +x install.sh
 #manual put in: File/Prefernces/Additional Board Manager URLs: http://arduino.esp8266.com/stable/package_esp8266com_index.json
 #Arduino IDE Tools/Board/Board Manager and install package (in my case esp8266 by ESP8266 Community)
 echo 
-echo "- For continu install"
-echo "need make some setting in Arduino IDE, after which we will proceed with the installation dependencies on github"
-echo "for contunuu open Arduino IDE and put in Menu File/Prefernces/Additional Board Manager: "
-echo "http://arduino.esp8266.com/stable/package_esp8266com_index.json"
-echo "and in Menu Tools/Board/Board Manager click and install package (in my case esp8266 by ESP8266 Community)"
-echo "after close Arduino IDE and press y in terminal"
+echo "- For continu install:"
+echo " need make some setting in Arduino IDE, after which we will proceed with the installation dependencies on github"
+echo " for contunuu open Arduino IDE and put in Menu File/Prefernces/Additional Board Manager: "
+echo " http://arduino.esp8266.com/stable/package_esp8266com_index.json"
+echo " and in Menu Tools/Board/Board Manager click and install package (in my case esp8266 by ESP8266 Community)"
+echo " after close Arduino IDE and press y in terminal"
 echo 
-echo "- For exit to install press n"
+echo "- For exit to install: press n"
 read pa
 if [ "$pa" != "y" ]; then
   exit;
