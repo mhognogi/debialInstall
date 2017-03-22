cf3cb428962ce6f0a50448901770200de3a26492
Marius <mhognogi@users.noreply.github.com>
2017-03-22 16:51:56 +0200

    Update java8.sh

--- java8.sh
+++ java8.sh
@@ -5,7 +5,5 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
 
-echo 
-echo "Installed Java 8 success"
-echo
+echo "\nJava 8 was installed\n"
 java -version
