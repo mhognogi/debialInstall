429f0463e1e48b85cb0e7ce0292cb713965d588c
Marius <mhognogi@users.noreply.github.com>
2017-04-06 17:32:43 +0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -20,9 +20,12 @@ service apache2 restart
 sudo apt-get -y install phpmyadmin
 sudo gedit /etc/apache2/apache2.conf
 echo $'\nPlease include in file apache2.conf: Include /etc/phpmyadmin/apache.conf\n'
-service apache2 restart
+sudo service apache2 restart
 #open localhost/phpmyadmin with root and your pass 
 
 #show error
 echo $'\nedit two line in sudo gedit /etc/php/7.0/apache2/php.ini\ndisplay_errors = on\ndisplay_startup_errors = on\n and save file\n'
-service apache2 restart
+sudo service apache2 restart
+
+#xdebug
+sudo apt-get install php-xdebug
