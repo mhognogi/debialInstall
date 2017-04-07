6fe4f952b9a5a0b5e840ff9af541f31a328eafea
Marius <mhognogi@users.noreply.github.com>
2017-04-07 14:21:54 +0300

    Update phpstorm.sh

--- phpstorm.sh
+++ phpstorm.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#install phpStorm from PhpStorm-2016.3.3.tar.gz
+#install phpStorm from PhpStorm-2017.1.1.tar.gz
 
 cd ~/Downloads
-wget http://download-cf.jetbrains.com/webide/PhpStorm-2016.3.3.tar.gz
+wget http://download-cf.jetbrains.com/webide/PhpStorm-2017.1.1.tar.gz
 sudo tar xf PhpStorm-*.tar.gz -C /opt/
 cd /opt/PhpStorm-*/bin
 ./phpstorm.sh
