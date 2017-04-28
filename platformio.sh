1d1c3499bf440f2845a20291492961c235447748
Marius <mhognogi@users.noreply.github.com>
2017-04-28 13:15:28 +0300

    Update platformio.sh

--- platformio.sh
+++ platformio.sh
@@ -11,8 +11,6 @@ sudo apt-get install atom
 #open atom
 atom
 
-echo ""
-echo ""
-echo "!! install manual package: platformio-ide (Setting/Packages/)"
-echo "!! then fix errors"
-echo "!! and run in terminal: sudo chmod a+rw /dev/ttyUSB0"
+echo $'\n\n!! install manual package: platformio-ide (Setting/Packages/)'
+echo $'!! then fix errors'
+echo $'!! and run in terminal: sudo chmod a+rw /dev/ttyUSB0'
