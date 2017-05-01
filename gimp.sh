319034438260a9799237054c298f421666e64c07
Marius <mhognogi@users.noreply.github.com>
2017-05-01 15:42:05 +0300

    Create gimp.sh

---
+++ gimp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get purge gimp
+sudo apt-get install ppa-purge 
+sudo ppa-purge ppa:otto-kesselgulasch/gimp  
+sudo ppa-purge ppa:otto-kesselgulasch/gimp-edge
+sudo apt-get update
+sudo apt-get install gimp
