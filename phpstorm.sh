4151a62a757087a3b1519637c6ee7e5b631ae5ac
Marius <mhognogi@users.noreply.github.com>
2019-03-20 16:48:44 +0200

    Update phpstorm.sh

--- phpstorm.sh
+++ phpstorm.sh
@@ -5,5 +5,5 @@ cd ~/Downloads
 #wget http://download-cf.jetbrains.com/webide/PhpStorm-201*.tar.gz
 sudo tar xf PhpStorm-*.tar.gz -C /opt/
 sudo tar xf datagrip-201*.tar.gz -C /opt/
-lscd /opt/PhpStorm-*/bin
+cd /opt/PhpStorm-*/bin
 ./phpstorm.sh
